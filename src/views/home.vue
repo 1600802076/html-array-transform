b4a4929e6fd59f87de40f9b509c07e9d898df527
1600802076 <1430578107@qq.com>
2021-08-11 00:31:11 +0800

    first commit

---
+++ src/views/home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div :class="$style.vueName"></div>
+</template>
+<script>
+export default {
+  name: 'vueName',
+  mixins: [],
+  components: {},
+  props: {
+  },
+  data () {
+    return {
+    }
+  },
+  computed: {
+  },
+  watch: {
+  },
+  created () {
+  },
+  mounted () {
+  },
+  methods: {
+  },
+  beforeDestroy () {
+  }
+}
+</script>
+<style module lang="scss">
+.vueName {
+  :global {
+  }
+}
+</style>
